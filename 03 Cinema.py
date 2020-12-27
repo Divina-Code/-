9a75a602ed6af7fc4d183fd4ecc5967c98156f37
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-27 18:51:52 +0300

    2

--- 03 Cinema.py
+++ 03 Cinema.py
@@ -4,7 +4,7 @@ print("Hello, user!")
 user_name = input("What is your name?   ")
 user_eayr = int (input("Whan you was born?   "))
 user_age = 2020 - user_eayr
-if user_age > 18:
-    print(user_name,"let's watch an action movie")
+if user_age >= 18:
+    print(user_name+",let's watch an action movie")
 else:
-    print(user_name,"let's watch a Comedy")
+    print(user_name+",let's watch a Comedy")
