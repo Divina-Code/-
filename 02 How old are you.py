b636ac6dda19cae857ea8fdca880c68bdf173248
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-27 15:29:40 +0300

    Hello

--- 02 How old are you.py
+++ 02 How old are you.py
@@ -1,3 +1,11 @@
 print("Hello, user!")
+
+
 user_name = input("What is your name?   ")
 print("Hello,", user_name)
+
+
+user_eayr = int (input("Whan you was born?   "))
+print("nise eyar,", user_eayr)
+
+print(2020-user_eayr)
