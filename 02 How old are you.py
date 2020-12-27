e3a72844e4d55962c17498af8af4c967f69f7cd6
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-27 16:24:20 +0300

    Update 02 How old are you.py

--- 02 How old are you.py
+++ 02 How old are you.py
@@ -2,10 +2,12 @@ print("Hello, user!")
 
 
 user_name = input("What is your name?   ")
-print("Hello,", user_name)
+print("Hello,", len(user_name), 'letters')
 
 
 user_eayr = int (input("Whan you was born?   "))
-print("nise eyar,", user_eayr)
-
-print(2020-user_eayr)
+if len(str(user_eayr)) == 4:
+    print("nise eyar,", user_eayr)
+    print(2020-user_eayr)
+else:
+    print("you make mistake")
