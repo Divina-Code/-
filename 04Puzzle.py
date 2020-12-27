1b84f884310ceee6f2e1b66db631199af6c89fb8
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-27 18:52:27 +0300

    Revert "Revert "Create 04Puzzle.py""
    This reverts commit 3adde1da806d16d5e8a174ee26ee6e0fe9781aa1.

---
+++ 04Puzzle.py
@@ -0,0 +1,15 @@
+
+isGuessed = False
+
+while isGuessed != True:
+
+    riddle = "\nБыло 6 друзей, на сколько равных частей надо разрезать торт, что бы всем досталось одинаковое количество торта "
+
+    answer = input(riddle)
+
+    if answer == "6":
+        print("правельно")
+        isGuessed = True
+    else:
+        print("ты не прав, попробуй ещё раз")
+print("спасибо за игру")
