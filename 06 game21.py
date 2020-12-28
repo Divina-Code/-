976b99e89709050f74a83084005f950d75bcb0ed
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-28 19:49:50 +0300

    Update 06 game21.py

--- 06 game21.py
+++ 06 game21.py
@@ -14,24 +14,24 @@ while inGame1 and inGame2:
 
     #ПРЕДЛАГАЕМ КАРТУ 1-МУ игроку
     if inGame1 and inGame2:
-        take_card = input("Будешь брать карту? [Да\Нет]  ")
+        take_card = str(input(player1Name + ", будешь брать карту? Да\Нет  "))
         if take_card == "Да":
             kard1 = random.randint(2, 11)
             print("карта = ", kard1)
             player1 += kard1
-            print(player1Name, "Теперь у тебя очков", player1)
+            print(player1Name, ", теперь у тебя очков", player1)
         elif take_card == "Нет":
             inGame1 = False
         else:
             print("Я тебя не понял")
 
             #ПРЕДЛАГАЕМ КАРТУ 2-МУ ИГРОКУ
-        take_card = input("Будешь брать карту? [Да\Нет]  ")
+        take_card = str(input(player2Name + ", будешь брать карту? Да\Нет  "))
         if take_card == "Да":
             kard2 = random.randint(2, 11)
             print("карта = ", kard2)
             player2 += kard2
-            print(player2Name, "Теперь у тебя очков", player2)
+            print(player2Name, ", теперь у тебя очков", player2)
         elif take_card == "Нет":
             inGame2 = False
         else:
@@ -41,6 +41,8 @@ if player1 == 21:
     print(player1Name, "Ты победил")
 elif player1 > player2:
     print(player1Name, "Ты победил")
+elif player1 > 21:
+    print(player2Name, "Ты победил")
 else:
     print(player2Name, "Ты победил")
 
