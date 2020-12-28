4249bb4e4b7a55e6a38d7ab4df8d901a3c81fdab
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-28 19:42:03 +0300

    Update 06 game21.py

--- 06 game21.py
+++ 06 game21.py
@@ -1,39 +1,47 @@
 import random
 
 player1 = random.randint(2, 11)
-print("Твоя карта = ", player1)
+player1Name = input("введи своё имя  ")
+print(player1, "= твоя карта")
 player2 = random.randint(2, 11)
-print("Твоя карта = ", player2)
+player2Name = input("введи своё имя  ")
+print(player1, "= твоя карта")
 
+inGame1 = True
+inGame2 = True
 
-inGame1 = True #Играт ли игрок1 или уже закончил
-
-
-
-
-while inGame1:
+while inGame1 and inGame2:
 
     #ПРЕДЛАГАЕМ КАРТУ 1-МУ игроку
-    if inGame1:
-        take_card = input("Будешь брать карту? [ДА\НЕТ]")
-        if take_card == "ДА":
+    if inGame1 and inGame2:
+        take_card = input("Будешь брать карту? [Да\Нет]  ")
+        if take_card == "Да":
             kard1 = random.randint(2, 11)
             print("карта = ", kard1)
             player1 += kard1
-            print("Теперь у тебя очков", player1)
-        elif take_card == "НЕТ":
+            print(player1Name, "Теперь у тебя очков", player1)
+        elif take_card == "Нет":
             inGame1 = False
         else:
             print("Я тебя не понял")
 
-    #Проверяем, не перебрал ли игрок карт
-    if player1>=21:
-        inGame1 = False
-
-print("Игра окончена")
-
-#ИЩЕМ ПОБЕДИТЕЛЯ
-if player1 <= 21:
-    print("Ты победил! Ура!")
+            #ПРЕДЛАГАЕМ КАРТУ 2-МУ ИГРОКУ
+        take_card = input("Будешь брать карту? [Да\Нет]  ")
+        if take_card == "Да":
+            kard2 = random.randint(2, 11)
+            print("карта = ", kard2)
+            player2 += kard2
+            print(player2Name, "Теперь у тебя очков", player2)
+        elif take_card == "Нет":
+            inGame2 = False
+        else:
+            print("Я тебя не понял")
+# ИЩЕМ ПОБЕДИТЕЛЯ
+if player1 == 21:
+    print(player1Name, "Ты победил")
+elif player1 > player2:
+    print(player1Name, "Ты победил")
 else:
-    print("Ты проиграл, увы")
+    print(player2Name, "Ты победил")
+
+
