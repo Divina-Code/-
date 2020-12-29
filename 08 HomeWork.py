80ee02686d80d5b1affb5b4fe6499a1b9478826c
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-29 10:32:35 +0300

    Create 08 HomeWork.py

---
+++ 08 HomeWork.py
@@ -0,0 +1,18 @@
+inList = False #продолжительность цикла
+productlist = []
+while inList != True:
+    answer = int(input("добавить продукт в список:[1], удалить:[2] или посмотреть весь список?:[3]  "))
+    if answer == 1:
+        product = input("что ты хочешь добавить?  ")
+        productlist.append(product)
+
+    elif answer == 2:
+        delete = int(input("что ты хочешь удалить?  "))
+        productlist.pop(delete)
+
+    elif answer == 3:
+        print(productlist)
+else:
+    print("выберите 1, 2 или 3")
+
+
