47c7f59844bb5881801fdb3e628e31ad97fc2c60
haker1235 <grebenkin.ilya07@gmail.com>
2020-12-29 12:06:31 +0300

    Update 06 game21.py

--- 06 game21.py
+++ 06 game21.py
@@ -1,20 +1,24 @@
 import random
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+print(Fore.RED + 'Красный текст')
+
+
 
 player1 = random.randint(2, 11)
 player1Name = input("Введи своё имя  ")
 print(player1, "= твоя карта")
-player2 = random.randint(2, 11)
-player2Name = input("Введи своё имя  ")
-print(player1, "= твоя карта")
+
 
 inGame1 = True
-inGame2 = True
 
-while inGame1 and inGame2:
+
+while inGame1:
 
     #ПРЕДЛАГАЕМ КАРТУ 1-МУ игроку
-    if inGame1 and inGame2:
-        take_card = str(input(player1Name+", будешь брать карту? Да\Нет  "))
+    if inGame1:
+        take_card: str = str(input(player1Name+", будешь брать карту? Да\Нет  "))
         if take_card == "Да":
             kard1 = random.randint(2, 11)
             print("карта = ", kard1)
@@ -25,26 +29,43 @@ while inGame1 and inGame2:
         else:
             print("Я тебя не понял")
         if player1 > 21:
-            print(player2Name+", ты победил")
+            print(player1Name+", ты проиграл")
+            inGame1 = False
+        elif player1 == 21:
+            print(player1Name+", ты победил!!!")
 
-            #ПРЕДЛАГАЕМ КАРТУ 2-МУ ИГРОКУ
-        take_card = str(input(player2Name+", будешь брать карту? Да\Нет  "))
-        if take_card == "Да":
+colorama.init()
+print(Fore.BLUE + 'Синий текст')
+
+
+inGame2 = True
+
+
+player2 = random.randint(2, 11)
+player2Name = input("Введи своё имя  ")
+print(player2, "= твоя карта")
+
+while inGame2:
+
+    #ПРЕДЛАГАЕМ КАРТУ 2-МУ игроку
+    if inGame2:
+        take_card2: str = str(input(player2Name+", будешь брать карту? Да\Нет  "))
+        if take_card2 == "Да":
             kard2 = random.randint(2, 11)
             print("карта = ", kard2)
             player2 = kard2 + player2
             print(player2Name, ", теперь у тебя очков", player2)
-        elif take_card == "Нет":
+        elif take_card2 == "Нет":
             inGame2 = False
         else:
             print("Я тебя не понял")
-            if player2 > 21:
-                print(player1Name+", ты победил")
-if player1 == 21:
-    print(player1Name, "Ты победил")
-elif player1 > player2:
-    print(player1Name, "Ты победил")
+        if player2 > 21:
+            print(player2Name+", ты проиграл")
+            inGame2 = False
+        elif player2 == 21:
+            print(player2Name+", Ты победил")
+if player1 > player2:
+    print("Победил, "+player1Name)
 else:
-    print(player2Name, "Ты победил")
-
+    print("Победил, "+player2Name)
 
